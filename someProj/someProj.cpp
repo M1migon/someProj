cb133c0b61e9a0615bf09cbe2e55271c5e70cd35
Mimigon <patramanskij@gmail.com>
2024-11-20 10:53:38 +0100

    Добавьте файлы проекта.

---
+++ someProj/someProj.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <random> 
+using namespace std;
+
+int randInt(int l, int k)
+{
+    return l + rand() % (k - l + 1);
+}  
+
+int cube(int a)
+{
+    return a * a * a;
+}
+
+int comp(int a, int b)
+{
+    return (a > b) ? (a) : (b);
+}
+
+bool signCheck(int a)
+{
+    return (a >= 0) ? (true) : (false);
+}
+
+bool primeNum(int a)
+{
+    return (a > 1 and a % 2 != 0 and a % 3 != 0 and a % 4 != 0) ? (true) : (false);
+}
+
+int main()
+{
+    cout << "F1 - " << cube(3) << "\n";
+    cout << "F2 - " << comp(11, 10) << "\n";
+    cout << "F3 - " << signCheck(-10) << "\n";
+    cout << "F4 - " << primeNum(11) << "\n";
+}
